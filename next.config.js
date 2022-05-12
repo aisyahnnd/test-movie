5a6163071d8990857ffaad9d81e763a8b782a1c0
aisyahnnd <aisyah214.ank@gmail.com>
2022-05-13 01:01:53 +0700

    first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+  serverRuntimeConfig: {
+    // Will be available on both server and client
+    apiKey: process.env.API_KEY,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiKey: process.env.API_KEY,
+  },
 }
 
 module.exports = nextConfig
